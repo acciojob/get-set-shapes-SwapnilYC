4026f2e03f354ccc0e2cece877cb7f79506220d5
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-13 18:45:45 +0530

    classes added with methods

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  get widtH() {
+    return this.width;
+  }
+  get heighT() {
+    return this.height;
+  }
+  getArea() {
+    return (this.height * this.width);
+  }
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+  getPerimeter() {
+    return (4 * super.width);
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
