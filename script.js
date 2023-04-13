c4d700edd6d8e0b5be2067229b5c9a1336d7ea3e
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-13 19:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 //complete this code
 class Rectangle {
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
+    this._width = width;
+    this._height = height;
   }
-  get widtH() {
-    return this.width;
+
+  get width() {
+    return this._width;
   }
-  get heighT() {
-    return this.height;
+
+  get height() {
+    return this._height;
   }
+
   getArea() {
-    return (this.height * this.width);
+    return this._width * this._height;
   }
 }
 
 class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
   getPerimeter() {
-    return (4 * super.width);
+    return 4 * this.width;
   }
 }
 
